65788521d2d5b16707bc34eef8637b80f96ad890
devluish <luis_unn@hotmail.com>
2024-11-17 15:46:27 -0300

    Commit Classes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.projeto.mobileglobal"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.projeto.mobileglobal"
---
+++ app/src/main/java/com/projeto/mobileglobal/MinhasReservasActivity.kt
@@ -0,0 +1,12 @@
+package com.projeto.mobileglobal
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MinhasReservasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_minhas_reservas)
+
+    }
+}
